c4eab07b81996f56cba8c882ca2ace896d87000f
ugra89 <ugra89@gmail.com>
2023-08-04 21:11:39 +0300

    error with guest update. solve later

--- index.js
+++ index.js
@@ -18,32 +18,126 @@ const mysqlConfig = {
   database: 'exam_schema',
 };
 
-const userSchema = Joi.object({
+const guestSchema = Joi.object({
   firstName: Joi.string().lowercase().required(),
   lastName: Joi.string().lowercase().required(),
   age: Joi.number().integer().min(1).max(100).required(),
   email: Joi.string().email().trim().lowercase().required(),
 });
 
+const userSchema = Joi.object({
+  name: Joi.string().lowercase().required(),
+  email: Joi.string().email().trim().lowercase().required(),
+  password: Joi.string().required(),
+});
+
 const dbPool = mysql.createPool(mysqlConfig).promise();
 
-server.get('/', (req, res) => {
-  res.send('loged in!');
-  const encryptedPassword = bcrypt.hash(payload.password, '123');
+server.get('/', authenticate, (req, res) => {
+  console.log(req.user);
+  res.status(200).send('loged in!');
 });
-server.get('/guests', (req, res) => {
-  res.send(guests);
+
+server.post('/register', async (req, res) => {
+  let payload = req.body;
+
+  try {
+    payload = await userSchema.validateAsync(payload);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send({ error: 'All fields are required' });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(payload.password, 10); // Čia hashuojamas slaptažodis su 10 slėptuvėmis
+    await dbPool.execute(
+      `
+            INSERT INTO user (name, email, password)
+            VALUES (?,?,?)`,
+      [payload.name, payload.email, hashedPassword]
+    );
+    res.status(201).json({ message: 'User added succesfuly.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
+});
+
+server.post('/login', async (req, res) => {
+  let payload = req.body;
+
+  try {
+    payload = await userSchema.validateAsync(payload);
+  } catch (error) {
+    console.error(error);
+    return res.status(400).send({ error: 'All fields are required' });
+  }
+  try {
+    const [data] = await dbPool.execute(
+      `
+            SELECT * FROM user
+           WHERE email=?
+            `,
+      [payload.email]
+    );
+    if (!data.length) {
+      return res
+        .status(400)
+        .send({ error: 'Email or password did not match 1' });
+    }
+    const isPasswordMatching = await bcrypt.compare(
+      payload.password,
+      data[0].password
+    );
+
+    console.log(isPasswordMatching);
+
+    if (isPasswordMatching) {
+      const token = jwt.sign(
+        {
+          email: data[0].email,
+          id: data[0].id,
+        },
+        process.env.JWT_SECRET
+      );
+
+      return res.status(200).send({ token });
+    }
+    return res.status(400).send({ error: 'Email or password did not match 2' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).end();
+  }
+});
+
+server.get('/guests', authenticate, async (req, res) => {
+  const userId = req.user.id;
+  try {
+    const [data] = await dbPool.execute(
+      `
+            SELECT * FROM guests
+            WHERE user_id=?
+            `,
+      [userId]
+    );
+    res.status(200).json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
 });
 
 server.post('/register-user', async (req, res) => {
   let payload = req.body;
+
   try {
-    payload = await userSchema.validateAsync(payload);
+    payload = await guestSchema.validateAsync(payload);
   } catch (error) {
     console.error(error);
 
     return res.status(400).send({ error: 'All fields are required' });
   }
+
   try {
     await dbPool.execute(
       `
@@ -56,33 +150,28 @@ server.post('/register-user', async (req, res) => {
     console.error(error);
     res.status(500).end();
   }
-  //   const newGuest = {
-  //     guest: {
-  //       firstName: payload.firstName,
-  //       lastName: ,
-  //       email: ,
-  //       age: ,
-  //     },
-  //     user: payload.user,
-  //   };
-  //   guests.push(newGuest);
-  //   );
 });
 
-server.put('/guests/:id', (req, res) => {
+server.put('/guests/:id', async (req, res) => {
   const payload = req.body;
   const id = req.params.id;
 
-  const updatedGuest = guests.find((guest) => guest.user === id);
+  const [result] = await dbPool.execute(
+    `
+        SELECT * FROM guests
+        WHERE id = ?
+      `,
+    [id]
+  );
+
+  const updatedGuest = result[0];
 
   if (updatedGuest) {
-    updatedGuest.guest = {
-      firstName: payload.firstName,
-      lastName: payload.lastName,
-      email: payload.email,
-      age: payload.age,
-    };
-    updatedGuest.user = payload.user;
+    updatedGuest['firstName'] = payload.firstName;
+    updatedGuest['lastName'] = payload.lastName;
+    updatedGuest['age'] = payload.age;
+    updatedGuest['email'] = payload.email;
+
     res.status(200).json({ message: 'Guest data is updated.' });
   } else {
     res.status(404).json({ message: 'Guest not found.' });
